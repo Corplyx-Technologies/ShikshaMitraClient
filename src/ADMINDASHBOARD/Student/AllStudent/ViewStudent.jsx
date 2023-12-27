215c3409022bf46710745dff8449c7d2adc0a299
Badal <badalkumarsingh7808@gmail.com>
2023-12-27 17:04:15 +0530

    Teacher and Student Download button

--- src/ADMINDASHBOARD/Student/AllStudent/ViewStudent.jsx
+++ src/ADMINDASHBOARD/Student/AllStudent/ViewStudent.jsx
@@ -3,8 +3,10 @@ import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { usePDF } from "react-to-pdf";
 import { FcLeft } from "react-icons/fc";
-import Cookies from 'js-cookie';
-const authToken = Cookies.get('token');
+import Cookies from "js-cookie";
+import generatePDF, { Resolution, Margin } from "react-to-pdf";
+
+const authToken = Cookies.get("token");
 
 // import "../../Dynamic/Form/FormStyle.css";
 
@@ -12,7 +14,44 @@ const ViewStudent = () => {
   const { email } = useParams();
   const [studentData, setStudentData] = useState({});
 
-  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
+  const [generatingPDF, setGeneratingPDF] = useState(false);
+  const options = {
+    // default is `save`
+    method: "open",
+    // default is Resolution.MEDIUM = 3, which should be enough, higher values
+    // increases the image quality but also the size of the PDF, so be careful
+    // using values higher than 10 when having multiple pages generated, it
+    // might cause the page to crash or hang.
+    resolution: Resolution.HIGH,
+    page: {
+      format: "a4", // Set the format to A4
+      // You can also adjust other properties like margin and orientation if needed
+      margin: Margin.SMALL,
+      orientation: "portrait", // or 'landscape' as per your requirement
+    },
+    canvas: {
+      // default is 'image/jpeg' for better size performance
+      mimeType: "image/png",
+      qualityRatio: 1,
+    },
+    // Customize any value passed to the jsPDF instance and html2canvas
+    // function. You probably will not need this and things can break,
+    // so use with caution.
+    overrides: {
+      // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+      pdf: {
+        compress: true,
+      },
+      // see https://html2canvas.hertzen.com/configuration for more options
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
+  const { toPDF, targetRef } = usePDF({
+    filename: "page.pdf",
+    options: options,
+  });
   console.log("How To Display ");
 
   useEffect(() => {
@@ -22,9 +61,9 @@ const ViewStudent = () => {
         `https://dull-rose-salamander-fez.cyclic.app/api/v1/adminRoute/getAllStudents?email=${email}`,
         {
           withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
         }
       )
       .then((response) => {
@@ -36,17 +75,38 @@ const ViewStudent = () => {
       });
   }, [email]);
 
+  const handlePDFGeneration = () => {
+    setGeneratingPDF(true); // Set state to true when PDF generation starts
+    setTimeout(() => {
+      toPDF();
+    }, 100);
+    // toPDF(); // Generate the PDF
+  };
+
+  const downloadButton = (
+    <button
+      onClick={handlePDFGeneration}
+      className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+    >
+      <svg
+        className="fill-current w-4 h-4 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+      </svg>
+      <span className="">Download</span>
+    </button>
+  );
+
   return (
     <>
-      
       <div className="   flex items-center justify-center pt-10 ">
         <div
           ref={targetRef}
           className="bg-white    gap-2 sm:p-4 md:p-4 lg:p-4 p-2 pt-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 shadow-[rgba(0,0,_0,_0.25)_0px_25px_50px-12px]   overflow-y-auto rounded-sm"
         >
-   
           <div className="  bg-[#01a9ac]  p-5   hover:shadow-[rgba(6,24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] rounded-md">
-          
             {console.log("student")}
             <div className=" flex justify-center mt-4">
               {studentData.image && studentData.image.url ? (
@@ -74,22 +134,7 @@ const ViewStudent = () => {
                 <p className=" p-2 font-bold">{`Address : ${studentData.address}`}</p>
               </div>
               <div className="flex justify-center mt-3 ">
-                <button
-                  onClick={() => {
-                    console.log("Button Clicked ")
-                    toPDF();
-                  }}
-                  className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-                >
-                  <svg
-                    class="fill-current w-4 h-4 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                  </svg>
-                  <span className="">Download</span>
-                </button>
+                {!generatingPDF && downloadButton}
               </div>
             </div>
           </div>
--- src/ADMINDASHBOARD/Teacher/ViewTeacher.jsx
+++ src/ADMINDASHBOARD/Teacher/ViewTeacher.jsx
@@ -2,17 +2,51 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-
+import generatePDF, { Resolution, Margin } from "react-to-pdf";
 import { FcLeft } from "react-icons/fc";
 import "../../Dynamic/Form/FormStyle.css";
 import { usePDF } from "react-to-pdf";
-import Cookies from 'js-cookie';
-const authToken = Cookies.get('token');
+import Cookies from "js-cookie";
+const authToken = Cookies.get("token");
 
 const ViewTeacher = () => {
   const { email } = useParams();
   const [teacherData, setTeacherData] = useState({});
   const { toPDF, targetRef } = usePDF({ filename: "Teacher.pdf" });
+  const [generatingPDF, setGeneratingPDF] = useState(false);
+  const options = {
+    // default is `save`
+    method: "open",
+    // default is Resolution.MEDIUM = 3, which should be enough, higher values
+    // increases the image quality but also the size of the PDF, so be careful
+    // using values higher than 10 when having multiple pages generated, it
+    // might cause the page to crash or hang.
+    resolution: Resolution.HIGH,
+    page: {
+      format: "a4", // Set the format to A4
+      // You can also adjust other properties like margin and orientation if needed
+      margin: Margin.SMALL,
+      orientation: "portrait", // or 'landscape' as per your requirement
+    },
+    canvas: {
+      // default is 'image/jpeg' for better size performance
+      mimeType: "image/png",
+      qualityRatio: 1,
+    },
+    // Customize any value passed to the jsPDF instance and html2canvas
+    // function. You probably will not need this and things can break,
+    // so use with caution.
+    overrides: {
+      // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+      pdf: {
+        compress: true,
+      },
+      // see https://html2canvas.hertzen.com/configuration for more options
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
 
   useEffect(() => {
     // Fetch teacher data based on the email parameter
@@ -21,9 +55,9 @@ const ViewTeacher = () => {
         `https://dull-rose-salamander-fez.cyclic.app/api/v1/adminRoute/getTeachers?email=${email}`,
         {
           withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      }, // Set withCredentials to true
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          }, // Set withCredentials to true
         }
       )
       .then((response) => {
@@ -36,11 +70,40 @@ const ViewTeacher = () => {
       });
   }, [email]);
 
+  const downloadButton = (
+    <button className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+      <svg
+        class="fill-current w-4 h-4 mr-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+      </svg>
+      <span className="">Download</span>
+    </button>
+  );
+
+  const handlePDFGeneration = () => {
+    setGeneratingPDF(true); // Set state to true when PDF generation starts
+
+    // Implement logic to generate PDF here
+    // Use the options and necessary logic to create the PDF
+    // For example:
+    setTimeout(() => {
+      toPDF(targetRef.current, options)
+        .then(() => {
+          setGeneratingPDF(false); // Reset state when PDF generation is done
+        })
+        .catch((error) => {
+          console.error("Error generating PDF:", error);
+          setGeneratingPDF(false); // Reset state even if there's an error
+        });
+    }, 100);
+  };
+
   return (
     <>
-      <div
-        className=" flex items-center justify-center pt-10 "
-      >
+      <div className=" flex items-center justify-center pt-10 ">
         <div
           ref={targetRef}
           className="bg-white  gap-2 sm:p-4 md:p-4 lg:p-4 p-2 pt-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 shadow-[rgba(0,0,_0,_0.25)_0px_25px_50px-12px]   overflow-y-auto"
@@ -81,20 +144,11 @@ const ViewTeacher = () => {
               </div>
               <div
                 onClick={() => {
-                  toPDF();
+                  handlePDFGeneration();
                 }}
                 className="flex justify-center mt-3 "
               >
-                <button className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                  <svg
-                    class="fill-current w-4 h-4 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                  </svg>
-                  <span className="">Download</span>
-                </button>
+                {!generatingPDF && downloadButton}
               </div>
             </div>
           </div>

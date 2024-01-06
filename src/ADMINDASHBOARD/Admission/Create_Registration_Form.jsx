43983e6766f5d7a9259fe2cafe31247a2aa7114b
Aman Sharma <halfengiiineeer2022@gmail.com>
2024-01-06 17:21:30 +0530

    Admin DashBoard finalized By Krishna Sir

--- src/ADMINDASHBOARD/Admission/Create_Registration_Form.jsx
+++ src/ADMINDASHBOARD/Admission/Create_Registration_Form.jsx
@@ -3,11 +3,11 @@ import InputForm from "../../Dynamic/Form/InputForm";
 import { toast } from "react-toastify";
 import Modal from "react-modal";
 import axios from "axios";
-import '../../Dynamic/Form/FormStyle.css'
+import "../../Dynamic/Form/FormStyle.css";
 import DynamicDataTable from "./DataTable";
 import { useStateContext } from "../../contexts/ContextProvider";
-import Cookies from 'js-cookie';
-const authToken = Cookies.get('token');
+import Cookies from "js-cookie";
+const authToken = Cookies.get("token");
 
 const modalStyle = {
   content: {
@@ -22,7 +22,7 @@ const modalStyle = {
 
 function Create_Registration_Form() {
   const { currentColor } = useStateContext();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     studentFullName: "",
     studentEmail: "",
@@ -50,29 +50,32 @@ function Create_Registration_Form() {
   const [submittedData, setSubmittedData] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    // Fetch data from the server when the component mounts
-    axios.get('https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/getLastYearStudents', {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      }, // Set withCredentials to true
-    })
-      .then((response) => {
-        if (Array.isArray(response.data.allStudent)) {
-          // Update the state with the array
-          setSubmittedData(response.data.allStudent);
-          console.log(response.data.allStudent);
-        } else {
-          console.error("Data format is not as expected:", response.allStudent);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        // Handle the error gracefully, e.g., show an error message to the user
-      });
-  }, [shouldFetchData]);
-
+  // useEffect(() => {
+  //   // Fetch data from the server when the component mounts
+  //   axios
+  //     .get(
+  //       "https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/getLastYearStudents",
+  //       {
+  //         withCredentials: true,
+  //         headers: {
+  //           Authorization: `Bearer ${authToken}`,
+  //         }, // Set withCredentials to true
+  //       }
+  //     )
+  //     .then((response) => {
+  //       if (Array.isArray(response.data.allStudent)) {
+  //         // Update the state with the array
+  //         setSubmittedData(response.data.allStudent);
+  //         console.log(response.data.allStudent[0].joiningDate);
+  //       } else {
+  //         console.error("Data format is not as expected:", response.allStudent);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //       // Handle the error gracefully, e.g., show an error message to the user
+  //     });
+  // }, [shouldFetchData]);
 
   const handleFieldChange = (fieldName, value) => {
     setFormData({
@@ -128,7 +131,7 @@ function Create_Registration_Form() {
           },
         }
       );
-      console.log(response.data)
+      console.log(response.data);
 
       setFormData({
         studentFullName: "",
@@ -155,7 +158,7 @@ function Create_Registration_Form() {
       setSubmittedData([...submittedData, formData]);
       setLoading(false);
       toast.success("Form submitted successfully!");
-      setShouldFetchData(!shouldFetchData)
+      setShouldFetchData(!shouldFetchData);
       closeModal();
     } catch (error) {
       console.error("Error:", error);
@@ -164,19 +167,25 @@ function Create_Registration_Form() {
     }
   };
 
-
   const handleDelete = (email) => {
-    axios.put(`https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/deactivateStudent`, { email }, {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    })
+    axios
+      .put(
+        `https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/deactivateStudent`,
+        { email },
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
       .then((response) => {
         console.log("Student data deleted successfully");
 
         // Update the state to remove the deleted data from the data table
-        const updatedData = submittedData.filter((item) => item.email !== email);
+        const updatedData = submittedData.filter(
+          (item) => item.email !== email
+        );
         setSubmittedData(updatedData);
 
         toast.success("Student data deleted successfully");
@@ -355,9 +364,50 @@ function Create_Registration_Form() {
     },
   ];
 
+  const handleSearch = () => {
+    const selectedYearElement = document.getElementById("yearDropdown");
+    const selectedYear = selectedYearElement.value;
+    console.log(selectedYear);
+
+    // Fetch data from the server when the component mounts
+    axios
+      .get(
+        "https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/getLastYearStudents",
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          }, // Set withCredentials to true
+        }
+      )
+      .then((response) => {
+        if (Array.isArray(response.data.allStudent)) {
+          // Filter students based on the joining date
+          const filteredStudents = response.data.allStudent.filter(
+            (student) => {
+              return student.joiningDate.split("-")[0] === selectedYear;
+            }
+          );
+
+          // Update the state with the filtered students
+          setSubmittedData(filteredStudents);
+          console.log("Filtered Students:", filteredStudents);
+        } else {
+          console.error("Data format is not as expected:", response.allStudent);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        // Handle the error gracefully, e.g., show an error message to the user
+      });
+  };
+
   return (
     <div className=" mt-12  w-[900px] mx-auto p-3">
-      <h1 className="text-2xl font-bold mb-4 uppercase text-center text-cyan-700"> New Registration</h1>
+      <h1 className="text-2xl font-bold mb-4 uppercase text-center text-cyan-700">
+        {" "}
+        New Registration
+      </h1>
       <button
         onClick={openModal}
         style={{ backgroundColor: currentColor }}
@@ -382,23 +432,65 @@ function Create_Registration_Form() {
           handleChange={handleFieldChange}
           handleImageChange={handleImageChange}
         />
-        <div style={{ display: "flex", justifyContent: "flex-end", padding: "10px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "10px",
+          }}
+        >
           <button
             onClick={handleSubmit}
             className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
           >
-            {loading ?
-              (<svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-              </svg>) : " Submit"
-            }
+            {loading ? (
+              <svg
+                aria-hidden="true"
+                class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            ) : (
+              " Submit"
+            )}
           </button>
-          <button onClick={closeModal} className="ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded">
+          <button
+            onClick={closeModal}
+            className="ml-2 bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded"
+          >
             Cancel
           </button>
         </div>
       </Modal>
+
+      <div className=" items-center">
+        <h2 className="text-xl font-semibold mt-4">New Registration</h2>
+        <select id="yearDropdown" className="m-2 w-[100px] md:mb-0 ">
+          {/* Populate the dropdown with 5 years starting from 2024 */}
+          {[...Array(5).keys()].map((index) => (
+            <option key={index} value={2023 + index}>
+              {2023 + index}
+            </option>
+          ))}
+        </select>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded m-1"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
 
       <DynamicDataTable data={submittedData} handleDelete={handleDelete} />
     </div>
--- src/ADMINDASHBOARD/Teacher/SalaryStatus.jsx
+++ src/ADMINDASHBOARD/Teacher/SalaryStatus.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Select from "react-select";
-import Cookies from 'js-cookie';
-const authToken = Cookies.get('token');
+import Cookies from "js-cookie";
+const authToken = Cookies.get("token");
 
 const SalaryStatus = () => {
   const { email } = useParams();
@@ -21,17 +21,20 @@ const SalaryStatus = () => {
 
   useEffect(() => {
     axios
-      .get(`https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/getTeachers?email=${email}`, {
-        withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-      })
+      .get(
+        `https://precious-pink-nightgown.cyclic.app/api/v1/adminRoute/getTeachers?email=${email}`,
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
       .then((response) => {
         const data = response.data.data[0];
         const id = response.data.data[0]._id;
         console.log("data", data);
-        setTeacherId(id)
+        setTeacherId(id);
         console.log("Id", id);
         setTeacherData(data);
       })
@@ -40,25 +43,60 @@ const SalaryStatus = () => {
       });
   }, [email]);
 
-  useEffect(() => {
-    if (teacherId) {
-      axios
-        .get(`https://precious-pink-nightgown.cyclic.app/api/v1/teacher/getPaymentHistory?teacherId=${teacherId}`, {
+  // useEffect(() => {
+  //   if (teacherId) {
+  //     axios
+  //       .get(
+  //         `https://precious-pink-nightgown.cyclic.app/api/v1/teacher/getPaymentHistory?teacherId=${teacherId}`,
+  //         {
+  //           withCredentials: true,
+  //           headers: {
+  //             Authorization: `Bearer ${authToken}`,
+  //           },
+  //         }
+  //       )
+  //       .then((response) => {
+  //         const data = response.data.data[0];
+  //         console.log("Salary", data);
+  //         console.log("Year", data.year);
+  //         setSalaryData(response.data.data[0].salaryHistory);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching Teacher salary data: ", error);
+  //       });
+  //   }
+  // }, [teacherId]);
+
+  const handleSearch = () => {
+    const selectedYearElement = document.getElementById("yearDropdown");
+    const selectedYear = selectedYearElement.value;
+
+    // Fetch salary data based on the selected year
+    axios
+      .get(
+        `https://precious-pink-nightgown.cyclic.app/api/v1/teacher/getPaymentHistory?teacherId=${teacherId}&year=${selectedYear}`,
+        {
           withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-        })
-        .then((response) => {
-          const data = response.data.data[0];
-          console.log("Salary", data);
-          setSalaryData(response.data.data[0].salaryHistory);
-        })
-        .catch((error) => {
-          console.error("Error fetching Teacher salary data: ", error);
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
+      .then((response) => {
+        const data = response.data.data;
+        const filteredData = data.map((item) => {
+          if (item.year === selectedYear) {
+            console.log(item.salaryHistory);
+            setSalaryData(item.salaryHistory);
+          }
+          return item;
         });
-    }
-  }, [teacherId]);
+        // console.log(filteredData);
+      })
+      .catch((error) => {
+        console.error("Error fetching Teacher salary data: ", error);
+      });
+  };
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
@@ -108,27 +146,33 @@ const SalaryStatus = () => {
     axios
       .post(apiUrl, newSalaryData, {
         withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
       })
       .then((response) => {
         console.log("Data Posted Successfully: ", response.data);
         // setSalaryData(response.data.salaryHistory);
         axios
-          .get(`https://precious-pink-nightgown.cyclic.app/api/v1/teacher/getPaymentHistory?teacherId=${teacherId}`, {
-            withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-          })
+          .get(
+            `https://precious-pink-nightgown.cyclic.app/api/v1/teacher/getPaymentHistory?teacherId=${teacherId}`,
+            {
+              withCredentials: true,
+              headers: {
+                Authorization: `Bearer ${authToken}`,
+              },
+            }
+          )
           .then((response) => {
             const data = response.data.data[0];
             console.log("Updated Salary", data);
             setSalaryData(response.data.data[0].salaryHistory);
           })
           .catch((error) => {
-            console.error("Error fetching updated Teacher salary data: ", error);
+            console.error(
+              "Error fetching updated Teacher salary data: ",
+              error
+            );
           });
       })
       .catch((error) => {
@@ -160,19 +204,34 @@ const SalaryStatus = () => {
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="modal p-4 bg-white rounded-lg shadow-lg md:w-2/3 lg:w-1/2">
             <div className="flex justify-between">
-              <span className="text-2xl font-semibold mb-4 text-indigo-600">Salary Form</span>
+              <span className="text-2xl font-semibold mb-4 text-indigo-600">
+                Salary Form
+              </span>
             </div>
             <div className="mb-4">
               <label className="block text-gray-600">Months</label>
               <Select
                 options={[
-                  "January", "February", "March", "April", "May", "June",
-                  "July", "August", "September", "October", "November", "December"
+                  "January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "September",
+                  "October",
+                  "November",
+                  "December",
                 ].map((month) => ({
                   value: month,
                   label: month,
                 }))}
-                value={selectedMonths.map((month) => ({ value: month, label: month }))}
+                value={selectedMonths.map((month) => ({
+                  value: month,
+                  label: month,
+                }))}
                 onChange={handleMonthsChange}
                 isMulti
                 placeholder="Select months"
@@ -216,7 +275,25 @@ const SalaryStatus = () => {
 
       {salaryData && (
         <div className="mt-8">
-          <h2 className="text-xl font-semibold mb-4">Salary History</h2>
+          <div className=" items-center">
+            <h2 className="text-xl font-semibold mb-4">Salary History</h2>
+            <select id="yearDropdown" className="mr-2 mb-2 md:mb-0 ">
+              {/* Populate the dropdown with 5 years starting from 2024 */}
+              {[...Array(5).keys()].map((index) => (
+                <option key={index} value={2023 + index}>
+                  {2023 + index}
+                </option>
+              ))}
+            </select>
+
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded m-1"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
+          </div>
+
           <table className="w-full border">
             <thead>
               <tr className="bg-gray-200">
